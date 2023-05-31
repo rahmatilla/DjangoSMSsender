0a9413523b1da6976cfe4019e5ed0a8e191e1762
rahmatilla <raxmatilla-89@mail.ru>
2023-05-31 11:25:21 +0500

    update 31.05.2023

--- config/settings.py
+++ config/settings.py
@@ -148,18 +148,18 @@ STATIC_URL = 'static/'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:4200"
+# ]
 
-CORS_ALLOW_HEADERS = (
-    "accept",
-    "authorization",
-    "content-type",
-    "user-agent",
-    "x-csrftoken",
-    "x-requested-with",
-)
+# CORS_ALLOW_HEADERS = (
+#     "accept",
+#     "authorization",
+#     "content-type",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+# )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
