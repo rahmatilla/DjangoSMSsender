a93c4d2924ef633fc699d878ee14178399b05ae0
rahmatilla <raxmatilla-89@mail.ru>
2023-05-31 10:31:16 +0500

    update 31.05.2023

--- config/settings.py
+++ config/settings.py
@@ -148,6 +148,10 @@ STATIC_URL = 'static/'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200"
+]
+
 CORS_ALLOW_HEADERS = (
     "accept",
     "authorization",
